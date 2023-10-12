f737538697aa60583c1618e56bbede1f7d44b4da
Vikas <vikaschauhaniam@gmail.com>
2023-10-12 17:09:30 +0530

    started working on Drup play game

---
+++ 5-DrumKit Game/index.js
@@ -0,0 +1,10 @@
+var numOfButton = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numOfButton; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("button "+i+1+" got clicked");
+    });
+}
+
+
+
