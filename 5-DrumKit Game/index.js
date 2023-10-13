6d31d16469681654bc0719ac65fe888d33925ac5
Vikas <vikaschauhaniam@gmail.com>
2023-10-13 10:54:59 +0530

    animation addition and clean code

--- 5-DrumKit Game/index.js
+++ 5-DrumKit Game/index.js
@@ -5,76 +5,73 @@ for(var i=0; i<numOfButton; i++){
       
         var buttonInnerHTMl = this.innerHTML;
 
-        switch (buttonInnerHTMl) {
-            case "w":
-                var instument = new Audio("sounds/tom-1.mp3");
-                instument.play();
-                break;
-            case "a":
-                var instument = new Audio("sounds/tom-2.mp3");
-                instument.play();
-                break;
-            case "s":
-                var instument = new Audio("sounds/tom-3.mp3");
-                instument.play();
-                break; 
-            case "d":
-                var instument = new Audio("sounds/tom-4.mp3");
-                instument.play();
-                break;
-            case "j":
-                var instument = new Audio("sounds/snare.mp3");
-                instument.play();
-                break;
-            case "k":
-                var instument = new Audio("sounds/crash.mp3");
-                instument.play();
-                break;
-            case "l":
-                var instument = new Audio("sounds/kick-bass.mp3");
-                instument.play();
-                break;                     
-            default:
-                break;
-        }
-        
+        makeSound(buttonInnerHTMl);
+        addAnimation(buttonInnerHTMl);
+
     });    
-        document.addEventListener("keydown", function(event){
-            switch (event.key) {
-                case "w":
-                    var instument = new Audio("sounds/tom-1.mp3");
-                    instument.play();
-                    break;
-                case "a":
-                    var instument = new Audio("sounds/tom-2.mp3");
-                    instument.play();
-                    break;
-                case "s":
-                    var instument = new Audio("sounds/tom-3.mp3");
-                    instument.play();
-                    break; 
-                case "d":
-                    var instument = new Audio("sounds/tom-4.mp3");
-                    instument.play();
-                    break;
-                case "j":
-                    var instument = new Audio("sounds/snare.mp3");
-                    instument.play();
-                    break;
-                case "k":
-                    var instument = new Audio("sounds/crash.mp3");
-                    instument.play();
-                    break;
-                case "l":
-                    var instument = new Audio("sounds/kick-bass.mp3");
-                    instument.play();
-                    break;                     
-                default:
-                    break;
-            }
-        });
+}
+
+document.addEventListener("keydown", function(event){
+
+    makeSound(event.key);
+    addAnimation(event.key);        
+});
+    
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var instument = new Audio("sounds/tom-1.mp3");
+            instument.play();
+            break;
+
+        case "a":
+            var instument = new Audio("sounds/tom-2.mp3");
+            instument.play();
+            break;
+
+        case "s":
+            var instument = new Audio("sounds/tom-3.mp3");
+            instument.play();
+            break; 
+
+        case "d":
+            var instument = new Audio("sounds/tom-4.mp3");
+            instument.play();
+            break;
+
+        case "j":
+            var instument = new Audio("sounds/snare.mp3");
+            instument.play();
+            break;
+
+        case "k":
+            var instument = new Audio("sounds/crash.mp3");
+            instument.play();
+            break;
+
+        case "l":
+            var instument = new Audio("sounds/kick-bass.mp3");
+            instument.play();
+            break;     
+
+        default:
+            break;
+    }
+}
+
+function addAnimation(key){
+
+    document.querySelector("."+key).classList.add("pressed"); 
+
+    
+    setTimeout(function() {
+        document.querySelector("."+key).classList.remove("pressed"); 
+    
+    }, 150);
     
 }
+
 
 
 
